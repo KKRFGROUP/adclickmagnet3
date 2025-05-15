bcc1ad311a25d028b5007f338ea76adcb7761f16
KKRF Group <168645376+KKRFGROUP@users.noreply.github.com>
2025-05-15 23:47:30 +0530

    Update DesignProcess.tsx

--- src/app/graphic-design/sections/DesignProcess.tsx
+++ src/app/graphic-design/sections/DesignProcess.tsx
@@ -8,7 +8,7 @@ const DesignProcess = () => {
           <h2 className="graphic-page-sec2-content-head">Captivating Graphic Design<br/> Services in the US & Canada<br/></h2>
           <p className="graphic-page-sec2-content-para">Whether you need an out-of-this-world illustration, beautiful print designs, or engaging digital marketing assets, ACM’s world-class graphic designers from around the world will make it happen.</p>
           <br />
-          <p className="graphic-page-sec2-content-para">Unlike traditional graphic design agencies, ACM is a tech enabled, Creative-as-a-Service design solution built to keep up with the evolving needs and fast pace of enterprise teams. Access a full suite of design services through a transparent design subscription model.</p>
+          <p className="graphic-page-sec2-content-para">As the best graphic design agency in the USA, ACM is a tech-enabled, Creative-as-a-Service design solution built to keep up with the evolving needs and fast pace of enterprise teams. Access a full suite of design services through a transparent design subscription model.</p>
         </div>
         <div className="flex justify-center graphic-page-sec2-graphic">
           <div className="graphic-page-sec2-graphic-card">
